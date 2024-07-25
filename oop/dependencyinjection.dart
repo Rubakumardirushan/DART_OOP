4580b1b2a2b66244e497366a354ce2d1db5b784f
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-07-25 11:00:17 +0530

    add

--- oop/dependencyinjection.dart
+++ oop/dependencyinjection.dart
@@ -1,20 +1,31 @@
 class animal {
   void name() {
-    print("Animal name is");
+    print("Animal name ");
+  }
+}
+
+class birds {
+  void name() {
+    print("Bird name ");
   }
 }
 
 class cat {
-  final animal ani;
-  cat(this.ani);
-
-  void showanimal() {
-    ani.name();
+  final animal? ani;
+  final birds? bir;
+  cat(this.ani, this.bir);
+  cat.withanimal(this.ani) : bir = null;
+  cat.withbirds(this.bir) : ani = null;
+  void name() {
+    ani?.name();
+    bir?.name();
   }
 }
 
 void main() {
   final animal a = animal();
-  final cat c = cat(a);
-  c.showanimal();
+  final birds b = birds();
+  final cat c = cat(a, b);
+
+  c.name();
 }
